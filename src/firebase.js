a98021455e33fd13f0ff5317384b6d86b5152de4
adrianrs928222 <adrianrs928222@gmail.com>
2026-05-09 02:02:37 +0200

    Actualizar firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,16 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBjfiEU_QpF6KBmWG_gjr-qpRmSai5Xpac",
-
   authDomain: "control-de-horaa.firebaseapp.com",
-
   projectId: "control-de-horaa",
-
-  storageBucket: "control-de-horaa.firebasestorage.app",
-
+  storageBucket:
+    "control-de-horaa.firebasestorage.app",
   messagingSenderId: "318630974127",
-
-  appId: "1:318630974127:web:cbd1010881f4632e63ba5a",
+  appId:
+    "1:318630974127:web:cbd1010881f4632e63ba5a",
 };
 
 const app = initializeApp(firebaseConfig);
